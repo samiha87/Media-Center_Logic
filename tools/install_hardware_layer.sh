0f755278159bc830206288679ca66dcd9789dcda
Sami Hamici <sami.hamici@corefactory.com>
2019-09-02 01:23:10 +0300

    Updated install hardware layer

---
+++ tools/install_hardware_layer.sh
@@ -0,0 +1,29 @@
+
+# Check if argument for ip has been set
+if [[ "$#" -eq 0 ]]; then
+	echo "User argument is missing, please pass user ./sync.sh <user> <IP_address>"
+	echo "IP address is optional, if not set using default 10.42.0.232"
+fi
+USER=$1
+RASPBERRY_IP=10.42.0.232
+if [[ $# < 2 ]]; 
+then 
+	echo "IP address not set, using default address"
+	echo "Default address 10.42.0.232"
+else
+	RASPBERRY_IP=$2
+fi
+echo "Installing hardware layer"
+scp -r -v ../hardwarehandler $USER@$RASPBERRY_IP:~/hardwarehandler/ > output.log 2>&1
+input="output.log"
+while IFS= read -r line
+do
+  	if [[ $line == *"Exit status 0"* ]];
+  		then
+			echo "Hardware layer installed"
+			rm output.log
+			exit 0
+	fi
+done < "$input"
+rm output.log
+echo "Installation failed"
